7cd0abcb84ea4edea129bbf9ae1fe55f81dc1651
Anka Markovic-Borak <cubicabubica@gmail.com>
2017-12-12 13:42:12 +0100

    Sredjivanje header-a i navigacije

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    
+    $('.navbar-nav a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 2000);
+    });
+    
+    
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 150) {
+            $('header').addClass('py-2');
+            $('header').css('background-color', 'rgba(0,0,0,0.8)');
+            $('.navbar-brand').addClass('small-logo');
+        } else {
+            $('header').removeClass('py-2');
+            $('header').css('background-color', 'rgba(0,0,0,0.3)');
+            $('.navbar-brand').removeClass('small-logo');
+        }
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
